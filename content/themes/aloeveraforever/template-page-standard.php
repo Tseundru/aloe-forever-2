8b0ad553de8e9c906b3cf0a42d1c390ff1221515
Tseundru <sergeant.ben@me.com>
2022-11-22 12:10:33 +0100

    template page standard4

--- content/themes/aloeveraforever/template-page-standard.php
+++ content/themes/aloeveraforever/template-page-standard.php
@@ -10,7 +10,9 @@ get_header();
 //dump(get_queried_object());
 
 ?>
-<main class="main">
+<main class="blogPost__main__content">
+  <h1 class="title title--1"><?php the_title(); ?></h1>
+
  <?php the_content(); ?>
 
 </main>
