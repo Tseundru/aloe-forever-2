22de7c5a3beb29fd4c31a96199571f59d27b221c
Tseundru <sergeant.ben@me.com>
2022-11-22 14:47:58 +0100

    template page standard css center title center

--- content/themes/aloeveraforever/template-page-standard.php
+++ content/themes/aloeveraforever/template-page-standard.php
@@ -12,7 +12,7 @@ get_header();
 ?>
 <main class="blogPost__main__content">
   <p></p>
-<h1 class="blogPost__header__title"><span><?php the_title(); ?></span></h1>
+<h1 class="blogPost__header__title is_center"><span><?php the_title(); ?></span></h1>
 
  <?php the_content(); ?>
 
