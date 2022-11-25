3ee6f2ecab3741835891db4083c7c6e80f86d7e0
Tseundru <sergeant.ben@me.com>
2022-11-25 07:05:30 +0100

    all page cat button shortcode2

--- content/themes/aloeveraforever/functions.php
+++ content/themes/aloeveraforever/functions.php
@@ -134,7 +134,9 @@ add_shortcode( 'MailLink', 'mailLink_func' );
 
 
 //shortcode catégories button title
-function page_cat_button_func($catName, $linkText ){
+function page_cat_button_func($atts){
+  $catName = $atts['cat'];
+  $linkText = $atts['text'];
   return '<a href="'.get_term_link($catName, Post_Type_Product::TAXONOMY_NAME_CATEGORY).'" class="button">'.$linkText.'.</a>';
 }
 add_shortcode( 'page_cat_button', 'page_cat_button_func' );
