d53619d047cc5a23a51da278d7c124cac88ffc58
Tseundru <sergeant.ben@me.com>
2022-11-25 06:35:05 +0100

    all products title

--- content/themes/aloeveraforever/template-parts/categorie-produit/categorie-produit.php
+++ content/themes/aloeveraforever/template-parts/categorie-produit/categorie-produit.php
@@ -61,7 +61,7 @@ if (isset($_POST) && $_POST != null) {
 <?php
 if(is_page(ALL_PRODUCTS_SLUG)){
   $imageID = get_post_thumbnail_id();
-  $catSubtitle = 'Tout pour votre bien être';
+  $catSubtitle = 'Tous les prodruits Forever';
   $catTitle = get_the_title();
   $catDescription = get_the_content() ;
 
