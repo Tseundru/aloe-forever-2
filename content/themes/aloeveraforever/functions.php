7521814f423a7f719e717b3a5f484e92d6cea88f
Tseundru <sergeant.ben@me.com>
2022-12-02 11:45:54 +0100

    bug fix

--- content/themes/aloeveraforever/functions.php
+++ content/themes/aloeveraforever/functions.php
@@ -220,62 +220,6 @@ function better_comments($comment, $args, $depth) {
         }
 endif;
 
-/** 
- * Google reCAPTCHA: Add widget before the submit button 
- */ 
-function add_google_recaptcha($submit_field) { 
-  $submit_field['submit_field'] = '<div class="g-recaptcha" data-sitekey="6LfElE0jAAAAACHRSqDM1nJRTutsYpYJLL8z0r4a"></div>'.$submit_field['submit_field']; 
-  return $submit_field; 
-} 
-
-if (!is_user_logged_in()) { 
-  add_filter('comment_form_defaults', 'add_google_recaptcha'); 
-} 
-
-/** 
-* Google reCAPTCHA: verify response and validate comment submission 
-*/ 
-function is_valid_captcha_response($captcha) { 
-  $captcha_postdata = http_build_query( 
-      array( 
-          'secret' => '6LfElE0jAAAAAE-Zi2ptV4p3tOpNryoKglyQmGG5', 
-          'response' => $captcha, 
-          'remoteip' => $_SERVER['REMOTE_ADDR'] 
-      ) 
-  ); 
-  $captcha_opts = array( 
-      'http' => array( 
-          'method'  => 'POST', 
-          'header'  => 'Content-type: application/x-www-form-urlencoded', 
-          'content' => $captcha_postdata 
-      ) 
-  ); 
-  $captcha_context  = stream_context_create($captcha_opts); 
-  $captcha_response = json_decode(file_get_contents("https://www.google.com/recaptcha/api/siteverify", false, $captcha_context), true); 
-  if(!empty($captcha_response['success'])){ 
-      return true; 
-  }else{ 
-      return false; 
-  } 
-} 
-
-function verify_google_recaptcha() { 
-  $recaptcha = $_POST['g-recaptcha-response']; 
-  if(empty($recaptcha)){ 
-      wp_die(__("<b>ERROR: </b><b>Please select captcha checkbox.</b><p><a href='javascript:history.back()'>« Back</a></p>")); 
-  }elseif(!is_valid_captcha_response($recaptcha)){ 
-      wp_die(__("<b>Sorry, spam detected!</b>")); 
-  } 
-} 
-
-if (!is_user_logged_in()) { 
-  add_action('pre_comment_on_post', 'verify_google_recaptcha'); 
-
-}
-
-
-
-
 // Acf return numeric fild as number and not a string
 add_filter('acf/format_value/type=number', 'acf_number_str_to_number', 20, 3);
 function acf_number_str_to_number($value, $post_id, $field) {
