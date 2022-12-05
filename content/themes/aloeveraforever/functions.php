86d077afbadf28fa9760e651641fc203af2a602c
Tseundru <sergeant.ben@me.com>
2022-12-05 13:15:35 +0100

    post count

--- content/themes/aloeveraforever/functions.php
+++ content/themes/aloeveraforever/functions.php
@@ -308,7 +308,7 @@ function gt_set_post_view() {
   $key = 'post_views_count';
   $post_id = get_the_ID();
   $count = (int) get_post_meta( $post_id, $key, true );
-  $count= $count+100;
+  $count++;
   update_post_meta( $post_id, $key, $count );
 }
 
