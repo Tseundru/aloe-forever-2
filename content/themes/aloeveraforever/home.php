1151fe478bb2d68e0347e390b992c164e39dfb30
Tseundru <sergeant.ben@me.com>
2022-12-22 11:08:30 +0100

    add text to home

--- content/themes/aloeveraforever/home.php
+++ content/themes/aloeveraforever/home.php
@@ -55,13 +55,49 @@ $ten_and_eleven = array_slice($terms_sort_by_order, 9, 2, true);
       <p>En effet, les avis clients sur Forever Living Products en France et dans le monde mettent toujours en avant la qualité des produits avec des notes qui avoisinent toujours les 5/5</p>
     </main>
   </article>
+  <article class="accordion">
+    <header class="accordion__header js-accordion">
+      <h2 class="accordion__header__title title title--2">
+        Aloe Vera Forever - La qualité avant tout !
+      </h2>
+      <i class="fa fa-plus accordion__header__icon" aria-hidden="true"></i>
+    </header>
+    <main class="accordion__main main">
+      <p>Le gel d’aloe vera ou pulpe d’aloès stabilisée que l’on retrouve dans la composition de la plupart des produits Forever Living est aussi le produit Forever le plus connu et vendu dans le monde.
+      </p>
+      <p>Initialement connu sous le nom de «bidon jaune», il doit sa réputation à son procédé de stabilisation unique qui permet de conserver les multiples vertus de l’aloe vera sans utiliser de conservateur chimique et nocif pour la santé. En effet la marque utilise simplement la technologie Tetrapak, de la vitamine C et de l’acide citrique pour conserver sa précieuse pulpe et empêcher son oxydation.
+      </p>
+      <p>La pulpe d’aloès de ce fameux «bidon jaune» est issue du gel interne des feuilles d’aloe vera. En effet, la substance gélatineuse, que l’on appelle mucilage et qui contient tous les principes actifs bénéfiques de la plante est séparée de la sève et de l’écorce qui ne sont pas adaptées à la consommation alimentaire.
+</p>
+<p>C’est ainsi que l’aloe vera de Forever conserve ses nombreuses propriétés pour le bien-être de toute la famille en usage interne et externe. Sa composition unique en vitamines et minéraux, enzymes digestives, acides aminés, anti-oxydants et nombreux principes actifs comme l’acémannane et les polysaccarides, lui confèrent beaucoup de bienfaits. Soutient le système immunitaire, le métabolisme et la digestion, favorise la production de collagène, améliore l’hydratation, apporte du tonus et de la vitalité en cas de fatigue, soulage les articulations, aide à la cicatrisation des plaies et renforce les défenses naturelles de l’organisme.
+</p>
+<h3>Une large gamme de produits naturels
+</h3>
+<p>Même si le coeur de métier de Forever Living Products est l’aloe vera, la marque sait très bien combiner les produits naturels aux dernières innovations technologiques dans les domaines du bien-être, de la perte de poids et des produits cosmétiques.
+</p>
+<p>Des buvables à l’aloe vera combinés avec des jus de fruits comme la canneberge dans Berry Nectar pour améliorer le confort urinaire, ou avec de la glucosamine et la chondroïtine dans Forever Freedom pour soulager les inconforts articulaires.
+</p>
+<p>Les produits de la ruche comme la gelée royale, le pollen, la propolis et le miel d’abeille. Mais aussi des compléments alimentaires comme la Maca, le calcium, les probiotiques, le ginkgo biloba et encore l’ail, le thym ou la vitamine C.
+</p>
+<p>L’aloe vera est également un ingrédient de choix pour les soins du visage du corps et des cheveux. Avec ses propriétés hydratantes qui pénètrent efficacement l’épiderme, il est souvent ajouté dans des soins du visage pour lutter contre le vieillissement en le combinant avec de la vitamine E. Shampoing, après-shampoing, crème visage, soins anti-âge, masque, savon, crème lavante, crème contours des yeux, sérum, démaquillant, lotion après rasage, baume à lèvre, huiles essentielles. 
+</p>
+<h3>Éthique, agriculture responsable et commerce équitable
+</h3>
+<p>Les produits Forever Living Products ne sont pas testé sur les animaux, en effets la marque dispose du label Leasing Bunny pour sont engagement dans la lutte contre la cruauté envers les animaux. 
+</p>
+<p>Les employés de la marque bénéficient toujours des meilleures conditions de travail possibles quelque soit le pays dans lequel ils oeuvrent. 
+</p>
+<p>Une agriculture responsable et un commerce équitable d’un bout à l’autre de la chaine, de la production à la commercialisation par les distributeurs, l’humain est toujours au centre des préoccupations de l’entreprise.
+</p>
+    </main>
+  </article>
 </section>
 
 
 <!-- Top Seller -->
 <section class="products topSeller">
   <header class="products__header">
-    <h2 class="products__header__title title title--2">Les plus vendus</h2>
+    <h2 class="products__header__title title title--2">Produits Forever les plus vendus</h2>
 
     <p class="products__header__subtitle">Vos produits Forever préférés, des produits qui ont faits leurs preuves
       <a href=<?= get_term_link('top-vente', 'product_badge'); ?> class="products__header__subtitle__link" title="Meilleures ventes des produits Forever">Voir Tous</a>
@@ -83,7 +119,7 @@ $ten_and_eleven = array_slice($terms_sort_by_order, 9, 2, true);
 <!-- New products -->
 <section class="products newProducts">
   <header class="products__header">
-    <h2 class="products__header__title title title--2">Nouveaux produits</h2>
+    <h2 class="products__header__title title title--2">Nouveaux produits Forever Living</h2>
     <p class="products__header__subtitle">
       Vos nouveaux produits préférés de la marque Forever Living
       <a href=<?= get_term_link('nouveaute', 'product_badge'); ?> class="products__header__subtitle__link" title="Nouveaux produits Forever">Voir Tous
@@ -105,7 +141,7 @@ $ten_and_eleven = array_slice($terms_sort_by_order, 9, 2, true);
 <!-- Aloe Max -->
 <section class="products aloeMax">
   <header class="products__header">
-    <h2 class="products__header__title title title--2">Teneur en Aloe Vera</h2>
+    <h2 class="products__header__title title title--2">Produits riche en Aloe Vera</h2>
     <p class="products__header__subtitle">
       Les produits à l'aloe vera de Forever avec une forte teneur en gel d'aloe vera
       <a href=<?= get_term_link('aloemax', 'product_badge'); ?> class="products__header__subtitle__link" title="Produits Forever à base d'aloe vera">Voir Tous
