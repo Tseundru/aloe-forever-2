dd04b225bddadb1e7725ef64184bccc4d988e003
Tseundru <sergeant.ben@me.com>
2023-01-10 22:17:47 +0100

    block image full bug test

--- content/themes/aloeveraforever/template-parts/blocks/construct/imageFull.php
+++ content/themes/aloeveraforever/template-parts/blocks/construct/imageFull.php
@@ -6,7 +6,7 @@ if (!get_field('block_construct_imageFull_image_with_title')):?>
   <?php write_responsive_image($imageId, 'full') ?>
   <?php else :?>
     <?php write_src_set_image($imageId, '.imageTitle'.$imageId); ?>
-  <div class="<?='imageTitle'.$imageId ?> <?= get_field('block_construct_imageFull_image_title_size') ?>">
+  <div class="<?='imageTitle imageTitle'.$imageId?> <?= get_field('block_construct_imageFull_image_title_size') ?>">
     <h2 class="imageTitle__title ">
       <?= get_field('block_construct_imageFull_image_title'); ?>
     </h2>
