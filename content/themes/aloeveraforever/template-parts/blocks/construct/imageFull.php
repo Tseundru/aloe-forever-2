8df3f5f4989259e9c1ea354313503540312547e0
Tseundru <sergeant.ben@me.com>
2023-01-10 22:13:51 +0100

    block image full bug test

--- content/themes/aloeveraforever/template-parts/blocks/construct/imageFull.php
+++ content/themes/aloeveraforever/template-parts/blocks/construct/imageFull.php
@@ -5,8 +5,8 @@ $imageId = get_field('block_construct_imageFull_image')['ID'];
 if (!get_field('block_construct_imageFull_image_with_title')):?>
   <?php write_responsive_image($imageId, 'full') ?>
   <?php else :?>
-    <?php write_src_set_image($imageId, '.imageTitle .image'.$imageId); ?>
-  <div class="<?='.imageTitle .image'.$imageId ?> <?= get_field('block_construct_imageFull_image_title_size') ?>">
+    <?php write_src_set_image($imageId, '.imageTitle'.$imageId); ?>
+  <div class="<?='imageTitle'.$imageId ?> <?= get_field('block_construct_imageFull_image_title_size') ?>">
     <h2 class="imageTitle__title ">
       <?= get_field('block_construct_imageFull_image_title'); ?>
     </h2>
