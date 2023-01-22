5a658f6953a9bf00652315274a80a51e4287d97d
Tseundru <sergeant.ben@me.com>
2023-01-22 13:45:01 +0100

    home content

--- content/themes/aloeveraforever/template-parts/posts/latestPost/latestPost.php
+++ content/themes/aloeveraforever/template-parts/posts/latestPost/latestPost.php
@@ -4,6 +4,6 @@
  <?php // write_src_set_image($image_ID , 'lastPosts__main__list__item') ?>
 <article class="lastPosts__main__list__item swiper-slide">
 <?php write_responsive_image($image_ID, 'large', 'lastPosts__main__list__item__img', 1); ?>
- <p class="lastPosts__main__list__item__title title title--3"><?= $post->post_title ?></p>
+ <span class="lastPosts__main__list__item__title title title--3"><?= $post->post_title ?></span>
  <div class="overlay"><a class="buttonOverlay buttonOverlay--readmore" href="<?php the_permalink($post->ID)?>" title="<?php the_title(); ?>">Lire la suite</a></div>
 </article>
