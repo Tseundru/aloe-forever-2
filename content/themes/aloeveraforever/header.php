fe850d6ddf80844c86858144ed9eab3a5214257c
Tseundru <sergeant.ben@me.com>
2023-01-28 09:09:54 +0100

    location page cities deleted little page

--- content/themes/aloeveraforever/header.php
+++ content/themes/aloeveraforever/header.php
@@ -75,7 +75,7 @@ $blog_categories = get_categories($args);
     if ($location_type === 'Ville'){
       if(!get_field('big_city')){
         echo '<meta name="robots" content="noindex, follow, nocache">';
-        echo'<meta http-equiv="refresh" content="durée;URL='.get_term_link($departementID).'">';
+        echo'<meta http-equiv="refresh" content="0;URL='.get_term_link($departementID).'">';
     }
   }
   }?>
